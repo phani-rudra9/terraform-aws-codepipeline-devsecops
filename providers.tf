f9b913eee1bb72838fdee30b564bf6e69d3edebb
System Administrator <root@CEOs-MacBook-Pro.local>
2022-08-07 20:06:06 +0530

    added shared creds file

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  shared_credentials_file = "/Users/ceophani/.aws/creds"
+  shared_credentials_file = "/Users/ceophani/.aws/credentials"
   profile                 = "default"
 }
